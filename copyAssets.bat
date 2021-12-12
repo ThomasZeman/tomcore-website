a40a945ae42beae899fe8acdb3c9e879288a591e
Thomas Zeman <git@thomas-zeman.com>
2021-12-12 23:16:22 +1100

    Change cache time to 1d

--- copyAssets.bat
+++ copyAssets.bat
@@ -1,1 +1,1 @@
-azcopy copy .\static\assets https://tciostorf57b640c1dbe9606.blob.core.windows.net/$web/assets --recursive --cache-control "max-age=1209600" --overwrite ifSourceNewer
+azcopy copy .\static\assets https://tciostorf57b640c1dbe9606.blob.core.windows.net/$web/assets --recursive --cache-control "max-age=86400" --overwrite ifSourceNewer
